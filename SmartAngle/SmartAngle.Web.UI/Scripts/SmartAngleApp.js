ce4d8ab706e0544ea6955166bcf9b250b41ed6b0
Gabriel <g.juarez14@hotmail.com>
2016-09-26 15:25:30 -0300

    Angularjs app initialized

---
+++ SmartAngle/SmartAngle.Web.UI/Scripts/SmartAngleApp.js
@@ -0,0 +1,26 @@
+﻿var smartAngleApp = angular.module('SmartAngleApp', ['ngRoute'])
+.run(function ($rootScope) {
+    $rootScope.SmartApiUri = "http://localhost:17289";
+    $rootScope.loggedIn = false;
+});
+
+smartAngleApp.config(['$routeProvider', '$locationProvider',
+     function ($routeProvider, $locationProvider, $rootScope, $location) {
+        $routeProvider.when('/', {
+            templateUrl: '/inicio.html'
+        }).when('/login', {
+            templateUrl: '/login.html'
+        }).when('/signUp', {
+            templateUrl: '/createUser.html'
+        }).when('/crearCuenta', {
+            templateUrl: '/createUser.html'
+        }).when('/panelDeControl', {
+            templateUrl: '/dashboard.html'
+        }).otherwise({
+            redirectTo: '/',
+            templateUrl: '/inicio'
+        });
+
+     }]);
+
+
